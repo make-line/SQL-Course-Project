4393b32b5b22fe7a88be987b8bd095c747b0d599
Madeline <47590770+make-line@users.noreply.github.com>
2020-05-15 14:50:19 +0300

    Add files via upload

---
+++ DateBase/DataBase/PrdcedVersion.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataBase
+{
+    /// <summary>
+    /// Логика взаимодействия для RectorVersion.xaml
+    /// </summary>
+    public partial class RectorVersion : Window
+    {
+        public RectorVersion()
+        {
+            InitializeComponent();
+        }
+    }
+}
